d0d9bb1684d5cf5720603f4967f8f47daffafb3a
Sylvia Jitti <72154164+SylviaJitti@users.noreply.github.com>
2023-04-17 17:40:22 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,95 +1,34 @@
 #!/usr/bin/python3
 """
-script based on the file 1-pack_web_static.py that distributes archive to
-webservers
+A script to add compressed file to server.
+The script will uncompress the files andserve the servers the index.html files.
 """
 import os.path
-from fabric.api import *
-from fabric.operations import run, put
-from datetime import datetime
+from fabric.api import run, put, env
 
-
-env.hosts = ['34.201.164.207', '54.84.88.116']
+env.hosts = ["34.201.164.207", "54.84.88.116"]
+env.key_filename = os.path.expanduser('~/.ssh/school')
 env.user = "ubuntu"
 
 
-def do_pack():
-    """ generates a .tgz archive from the contents of the web_static
-
-    All files in the folder web_static must be added to the final archive.
-    All archives must be stored in the folder versions.
-    The name of the archive created must be:
-        web_static_<year><month><day><hour><minute><second>.tgz
-    The function do_pack must return the archive path if the archive has
-    been correctly generated. Otherwise, it should return None.
-
-    Returns:
-        fabric.operations._AttributeString: archive path.
-    """
-    now = datetime.now().strftime("%Y%m%d%H%M%S")
-
-    # create folder versions if it doesn’t exist
-    local("sudo mkdir -p versions")
-
-    # extract the contents of a tar archive
-    result = local("tar -czvf versions/web_static_{}.tgz web_static"
-                   .format(now))
-    if result.failed:
-        return None
-    else:
-        return result
-
-
 def do_deploy(archive_path):
-    """distributes an archive to your web servers.
-
-    Args:
-        archive_path (string): path to archive
-
-    Returns:
-        Boolean: whether the archive is distributed or not
-    """
+    """Function that will run the script."""
     if not os.path.exists(archive_path):
         return False
-    # Uncompress the archive to the folder,
-    # /data/web_static/releases/<archive filename without extension>
-    # on the web server
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
-
-    try:
-        # upload the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/{}".format(file_name))
-
-        # Create new directory for release
-        run("sudo mkdir -p {}".format(folder_path))
-
-        # Untar archive
-        run("sudo tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-
-        # Delete the archive from the web server
-        run("sudo rm -rf /tmp/{}".format(file_name))
-
-        # Move extraction to proper directory
-        run("sudo mv {}web_static/* {}".format(folder_path, folder_path))
-
-        # Delete first copy of extraction after move
-        run("sudo rm -rf {}web_static".format(folder_path))
-
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("sudo rm -rf /data/web_static/current")
-
-        # Create new the symbolic link /data/web_static/current on web server,
-        # linked to the new version of your code,
-        # (/data/web_static/releases/<archive filename without extension>
-        run("sudo ln -s {} /data/web_static/current".format(folder_path))
-
-        print('New version deployed!')
-        success = True
-
-    except Exception:
-        success = False
-        print("Could not deploy")
-    return success
+    put(archive_path, "/tmp/")
+    compressed_filepath = "versions/web_static_20230406235730.tgz"
+    compressed_filename = compressed_filepath.split("/")[1]
+    compressed_file_without_ext = compressed_filename.split(".")[0]
+    folder_uncompressed_file = "/data/web_static/releases/{}".format(
+        compressed_file_without_ext)
+    run("sudo mkdir -p {}".format(folder_uncompressed_file))
+    run("sudo tar -xzf /tmp/{} -C {}".format(
+        compressed_filename, folder_uncompressed_file))
+    run("sudo rm /tmp/{} ".format(compressed_filename))
+    run("sudo mv {}/web_static/* {}".format(
+        folder_uncompressed_file, folder_uncompressed_file))
+    run("sudo rm -rf {}/web_static".format(folder_uncompressed_file))
+    run("sudo rm -rf /data/web_static/current")
+    run("sudo ln -s {} /data/web_static/current".format(
+        folder_uncompressed_file))
+    return True
